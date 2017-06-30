77793a4f4d63c67a2718ca5f3692771dc3c857ad
Roland Kullet <renaud.bellec.3@gmail.com>
2017-06-30 18:20:19 +0200

    initialized react

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+const path = require("path");
+
+module.exports = {
+    entry: {
+        app: "./static/js/index.js"
+    },
+    output: {
+        path: path.resolve(__dirname, "public/js"),
+        filename: "[name].dist.js"
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                use: [{
+                    loader: "babel-loader",
+                        options: {
+                            presets: ["es2015", "react"],
+                            plugins: ["transform-object-rest-spread"]
+                        }
+                }]
+            }
+        ]
+    },
+    devtool: "source-map"
+};
